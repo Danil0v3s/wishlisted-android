83b7d5f0c5f5625362cb49722c9b43929f4fae01
Danilo Lemes <daniloleemes@hotmail.com>
2021-04-12 19:29:57 +0100

    compose paging grid

--- app/src/main/java/app/wishlisted/android/app/home/deals/DealsScreen.kt
+++ app/src/main/java/app/wishlisted/android/app/home/deals/DealsScreen.kt
@@ -2,7 +2,10 @@ package app.wishlisted.android.app.home.deals
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,14 +15,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.itemsIndexed
+import androidx.paging.compose.items
 import app.wishlisted.android.domain.model.Game
 import app.wishlisted.android.domain.model.GameImagePurpose
 import app.wishlisted.android.domain.model.getImage
@@ -32,12 +34,34 @@ fun DealsScreen(
 ) {
     val dealsViewModel: DealsViewModel = hiltNavGraphViewModel()
     val deals = dealsViewModel.deals.collectAsLazyPagingItems()
+    val nColumns = 3
 
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
-        itemsIndexed(deals) { _, game ->
-            DealsContent(game!!, onItemClick)
+        items(deals) { game -> }
+
+        val rows = (deals.itemCount + nColumns - 1) / nColumns
+        items(rows) { rowIndex ->
+            Row {
+                for (columnIndex in 0 until nColumns) {
+                    val itemIndex = rowIndex * nColumns + columnIndex
+                    if (itemIndex < deals.itemCount) {
+                        Box(
+                            modifier = Modifier.weight(1f, fill = true),
+                            propagateMinConstraints = true
+                        ) {
+                            deals[itemIndex]?.let {
+                                DealsContent(game = it) {
+
+                                }
+                            }
+                        }
+                    } else {
+                        Spacer(Modifier.weight(1f, fill = true))
+                    }
+                }
+            }
         }
     }
 }
